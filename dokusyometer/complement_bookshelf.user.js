cb5fb18dd83da63ea5f8c5a08cca3cef83adf03b
nasu <tatu.nasu@gmail.com>
2011-01-22 21:50:06 +0900

    alpha version of complement-category@dokusyometer

---
+++ dokusyometer/complement_bookshelf.user.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name   読書メーターカテゴリ入力補完
+// @author nasu - http://www.google.co.jp/profiles/tatu.nasu#about
+// ==/UserScript==
+var obj = {
+  categories: [],
+  div_id: 'chrome-ext-complement-bookshelf',
+  category_class: 'chrome-ext-mycategory',
+  input_tag: $('input[type="text"][name="category"]'),
+  getCategories: function(){
+    var self = this;
+    if (! self.categories.length) {
+      var html = $.ajax({
+        url:"/u/31418/cat", 
+        async:false,
+      }).responseText;
+      $(html).find('#side_left > div.inner > ul > li').each(function(){
+        self.categories.push($(this).text().replace(/\(\d+\)$/, ''));
+      });
+    }
+    return self.categories;
+  },
+  show: function(){
+    var self = this;
+    $('#' + self.div_id).show();
+  },
+  hide: function(){
+    var self = this;
+    $('#' + self.div_id).hide();
+  },
+  ready: function(){
+    var self = this;
+    var area = $('div.book_edit_area');
+    area.append('<div id="' + self.div_id + '"></div>');
+    self.hide();
+
+    var categories = self.getCategories();
+    //TODO 半角スペースと全角スペースわかりやすく
+    var input_categories = self.input_tag.val().split(/[ 　]+/);
+    $.each(categories, function(){
+      var category = this.replace(/ /g, '');
+      if ($.inArray(category, input_categories) >= 0) {
+        //TODO rgb値を定数に
+        $('#' + self.div_id).append('<span class="' + self.category_class + '" style="background-color:rgb(255,255,0);">' + category + '</span>')
+      } else {
+        $('#' + self.div_id).append('<span class="' + self.category_class + '">' + category + '</span>')
+      }
+    });
+    $('.' + self.category_class).click(function(){obj.category_toggle(this)});
+    $('.' + self.category_class).css({
+      'margin':'0px 2px',
+      'cursor':'pointer',
+      'text-decoration':'underline',
+    });
+    self.show();
+  },
+  category_toggle: function(obj){
+    var self = this;
+    if ($(obj).css('background-color').replace(/ /g, '') == 'rgb(255,255,0)') {
+      $(obj).css({'background-color':'rgba(0,0,0,0)'});
+    } else {
+      $(obj).css({'background-color':'rgb(255,255,0)'});
+    }
+    //TODO たぶんmapでできる
+    //TODO 存在していないカテゴリを入力していると消える
+    var str = '';
+    $('.' + self.category_class).each(function(){
+      if ($(this).css('background-color').replace(/ /g, '') == 'rgb(255,255,0)') {
+        str += $(this).text() + ' ';
+      }
+    });
+    self.input_tag.val(str);
+  }
+}
+obj.ready();
